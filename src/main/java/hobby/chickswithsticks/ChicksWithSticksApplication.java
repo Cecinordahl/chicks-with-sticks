47ff0778ec3c0cdd85ca5f251e395f961d687a34
ceci <cecilie.nordahl@me.com>
2024-04-04 15:32:18 +0200

    Add index page and GET mapping

--- src/main/java/hobby/chickswithsticks/ChicksWithSticksApplication.java
+++ src/main/java/hobby/chickswithsticks/ChicksWithSticksApplication.java
@@ -2,6 +2,7 @@ package hobby.chickswithsticks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ChicksWithSticksApplication {
@@ -9,5 +10,8 @@ public class ChicksWithSticksApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChicksWithSticksApplication.class, args);
     }
-
+    @GetMapping("index")
+    public String frontPage(){
+        return "index";
+    }
 }

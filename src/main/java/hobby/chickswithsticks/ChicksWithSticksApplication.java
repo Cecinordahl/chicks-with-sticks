838a8d62c31384b1a383dfd23bca6974155b07ca
ceci <cecilie.nordahl@me.com>
2024-04-04 15:26:03 +0200

    Initial commit

---
+++ src/main/java/hobby/chickswithsticks/ChicksWithSticksApplication.java
@@ -0,0 +1,13 @@
+package hobby.chickswithsticks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicksWithSticksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicksWithSticksApplication.class, args);
+    }
+
+}
